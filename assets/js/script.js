9fbf76e3f1375ab7abd6c3963e072f2d67f5d7bb
0w3nM <>
2023-10-17 03:40:06 +0000

    Updated JavaScript code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,104 @@ function randomChoice() {
 /* Game function */
 function playGame(userChoice) {
     const opponentChoice = randomChoice();
+    if (userChoice === opponentChoice) {
+        alert("DRAW");
+    }
+
+    else if (userChoice == 'rock') {
+        if (opponentChoice == 'paper') {
+            alert("LOSE");
+            opponent++;
+        } else if (opponentChoice == 'scissors') {
+            alert('WIN');
+            scores++;
+        } else if (opponentChoice == 'lizard') {
+            alert('WIN');
+            scores++;
+        } else if (opponentChoice == 'spock') {
+            alert('Lose');
+            opponent++;
+        }
+    }
+
+    else if (userChoice == 'paper') {
+        if (opponentChoice == 'rock') {
+            alert("WIN");
+            scores++;
+        } else if (opponentChoice == 'scissors') {
+            alert('LOSE');
+            scores++;
+        } else if (opponentChoice == 'lizard') {
+            alert('lOSE');
+            opponent++;
+        } else if (opponentChoice == 'spock') {
+            alert('WIN');
+            scores++;
+        }
+    }
+
+    else if (userChoice == 'scissors') {
+        if (opponentChoice == 'paper') {
+            alert("WIN");
+            scores++;
+        } else if (opponentChoice == 'rock') {
+            alert('LOSE');
+            opponent++;
+        } else if (opponentChoice == 'lizard') {
+            alert('WIN');
+            scores++;
+        } else if (opponentChoice == 'spock') {
+            alert('LOSE');
+            opponent++;
+        }
+    }
+
+    else if (userChoice == 'lizard') {
+        if (opponentChoice == 'paper') {
+            alert("WIN");
+            scores++;
+        } else if (opponentChoice == 'scissors') {
+            alert('LOSE');
+            opponent++;
+        } else if (opponentChoice == 'rock') {
+            alert('LOSE');
+            opponent++;
+        } else if (opponentChoice == 'spock') {
+            alert('WIN');
+            scores++;
+        }
+    }
+
+    else if (userChoice == 'spock') {
+        if (opponentChoice == 'paper') {
+            alert("LOSE");
+            opponent++;
+        } else if (opponentChoice == 'scissors') {
+            alert('WIN');
+            scores++;
+        } else if (opponentChoice == 'lizard') {
+            alert('LOSE');
+            opponent++;
+        } else if (opponentChoice == 'rock') {
+            alert('WIN');
+            scores++;
+        }
+    }
+
+    turns++;
+    document.getElementById("tries").textContent = turns;
+    document.getElementById("player-score").textContent = scores;
+    document.getElementById("opponentChoice").textContent = opponent;
+
+    if (turns === 5) {
+        if (scores >= 3) {
+            alert("GAME OVER YOU WIN !!");
+        } else {
+            alert("GAME OVER YOU LOSE !!");
+        }
+        replayGame();
+    }
+}   
     
 
 /* Game replay */
