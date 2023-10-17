866e131a675b8cd7e4483dc13e4e3f9016be0276
0w3nM <>
2023-10-17 03:20:42 +0000

    Updated title name. Updated opponent class name. Added Favicon.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 /* Variable */
 let scores = 0;
+let opponent = 0;
 let turns = 0;
 const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
@@ -20,32 +21,7 @@ function randomChoice() {
 /* Game function */
 function playGame(userChoice) {
     const opponentChoice = randomChoice();
-    if (userChoice === opponentChoice) {
-        alert("DRAW");
-    } else if (userChoice == 'rock') {
-        if (opponent == 'paper') {
-            alert("LOSE");
-        } else if (opponent == 'scissors') {
-            alert('WIN');
-            scores++;
-        } else if (opponent == 'lizard') {
-            alert('WIN');
-            scores++;
-        }
-
-            turns++;
-            document.getElementById("tries").textContent = turns;
-            document.getElementById("player-score").textContent = scores;
-
-            if (turns === 5) {
-                if (scores >= 3) {
-                    alert("GAME OVER YOU WIN !!");
-                } else {
-                    alert("GAME OVER YOU LOSE !!");
-                }
-                replayGame();
-            }
-        }
+    
 
 /* Game replay */
 function replayGame() {
