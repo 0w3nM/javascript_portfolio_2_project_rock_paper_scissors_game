72aea6c2ac4c3f250d6db374f986a10bfd2019da
0w3nM <>
2023-10-17 15:31:54 +0000

    Added images for README.md as well as content, updated html issues brought on through validation, updated css on media queries 480px to accomidate the changes during gameplay.

--- README.md
+++ README.md
@@ -1,56 +1,83 @@
 ## Rock, Paper, Scicssors, Lizard, Spock
 
-
+The purpose of this site is to allow more people to play the updated version of the beloved game Rock,Paper, Scissors Game, that was introduced to us thanks to the sitcom The Big Bang Theory and ,some would say, that the game has been improved upon by offering more options to try and defeat your opponent.
+This game is targeted at all age groups that simply wish to pass the time with this game, challenge they're decision making skills, or introduce a new audience to the game.
 
 # -Features
 
-# Header
+- Header
+ ![Header](assets/images/js_header.png)
 
+* The header has Skranji font to make it stand out along with the font color being white on the dark part of the background color.
+
+- Input Box
+  ![Clear input field](assets/images/js_input.png)
+
+  * The background of the box is a gradient color of green to blue to make it stand out, with the font being darkred colored and a font-family of Skranji would make using the box more pleasant looking than and ordinary looking box.
+  
+  ![Input field needing content](assets/images/js_input_required.png)
+  - This image shows that the input field need to be written in to work.  
+
+* Game Options
+
+  ![Button options to choose](assets/images/js-choices.png)
+
+ - The buttons are aranged in a line for aesthetically pleasing to look at, spaced evenly. They have a background color that is closest to the choice, and an image from font awesome depicting the choice for users.
+
+* Game Results
+
+  ![Results](assets/images/js_scoring_boxes.png)
+
+- Each box has it numerical value in differnet colors for aethestics reason along with identifying them to their indivdual positions.
+- The boxes are aligned in a downward facing triangle to keep the them seperate and for aesthetic reasons.
 - 
-  ![Header](assets/images/)
+# - Technologies used
 
-# Game Options
-
-- 
-  ![Button options to choose](assets/images/)
-
-# Game Results
-
-- 
-
-  ![Results](assets/images/)
-
-# -Features left to implement
-
-- 
+- HTML
+- CSS
+- JavaScript
 
 # -Testing
 
-- 
-  ![lighthouse result](assets/images/)
-
-## Bugs-
+- ![lighthouse result](assets/images/lighthouse_validation.png)
 
 ## Validator Testing-
 
-- Html – Errors -  [W3C validator](https://validator.we.org/)
-  ![Html validator result](assets/images/)
+- Html – Errors - [W3C validator](https://validator.we.org/)
+  ![Html validator result](assets/images/html_validation.png)
 
-  JShint-  [JShint validator](https://jshint.com/)
+  JShint- [JShint validator](https://jshint.com/)
   ![JShint result](assets/images/)
 
 CSS –[W3C validator] (<https://jigsaw.w3.org/css-validator/>)
-![css result](assets/images/
+![css result](assets/images/css_validation.png)
+
 # Deployment-
 
-The game was deployed to GitHub pages.
+- The game was deployed to GitHub pages.
+- Live link to the page can be found here -
+  
+## Web Browser used -
+
+- Google Chrom
+- Microsoft Edge
+
+## Devices Used -
 
 # Credits-
 
 ## Content-
 
+* Inspiration for the project came from:
+
+  - [geeksforgeeks](https://www.geeksforgeeks.org/rock-paper-and-scissor-game-using-javascript/)
+  - [stackoverflow](https://stackoverflow.com/questions/22623331/rock-paper-scissors-lizard-spock-in-javascript/)
+  - [CodeInstitute](https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+JSE_PAGPPF+2021_Q2/courseware/30137de05cd847d1a6b6d2c7338c4655/c3bd296fe9d643af86e76e830e1470dd/)
 
 ## Media-
 
-- Icons for buttons the buttons were taken from [FontAwesome](https://fontawesome.com/)  
-- 
+- Icons for buttons the buttons were taken from [FontAwesome](https://fontawesome.com/)
+- Color was used from [w3school] (https://www.w3schools.com/colors/color_tryit.asp?hex=BC8F8F/),
+  (https://www.w3schools.com/css/css3_gradients.asp/)
+- For button positioning was looked at on [geeksforgeeks](https://www.geeksforgeeks.org/rock-paper-and-scissor-game-using-javascript/)
+- Form Validation was used from [CodeInstitute](https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+HE101+2020/courseware/fcc67a894619420399970ae84fc4802f/05a72bc3e032457d8a3d108d33656c72/?child=last/), (https://learn.codeinstitute.net/courses/course-v1:CodeInstitute+HE101+2020/courseware/fcc67a894619420399970ae84fc4802f/05a72bc3e032457d8a3d108d33656c72/?child=last/)
--- assets/js/script.js
+++ assets/js/script.js
@@ -13,133 +13,119 @@ document.getElementById("spock").addEventListener("click", playGame);
 
 /* Randomiser function */
 function randomChoice() {
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
 }
 
 /* Game function */
 function playGame(event) {
+  let userChoice = event.target.id;
+  const opponentChoice = randomChoice();
 
-    let userChoice = event.target.id;
-    const opponentChoice = randomChoice();
+  if (event.target.id) {
+    userChoice = event.target.id;
+  } else {
+    userChoice = event.target.parentElement.id;
+  }
 
-    if (event.target.id) {
-        userChoice = event.target.id;
+  if (userChoice === opponentChoice) {
+    alert("DRAW");
+  } else if (userChoice === "rock") {
+    if (opponentChoice === "paper") {
+      alert("Opponent chose paper YOU LOSE");
+      opponent++;
+    } else if (opponentChoice === "scissors") {
+      alert("Opponent chose scissors YOU WIN");
+      scores++;
+    } else if (opponentChoice === "lizard") {
+      alert("Opponent chose lizard YOU WIN");
+      scores++;
+    } else if (opponentChoice === "spock") {
+      alert("Opponent chose spock YOU LOSE");
+      opponent++;
     }
-    else {
-        userChoice = event.target.parentElement.id;
+  } else if (userChoice === "paper") {
+    if (opponentChoice === "rock") {
+      alert("Opponent chose rock YOU WIN");
+      scores++;
+    } else if (opponentChoice === "scissors") {
+      alert("Opponent chose scissors YOU LOSE");
+      scores++;
+    } else if (opponentChoice === "lizard") {
+      alert("Opponent chose lizard YOU LOSE");
+      opponent++;
+    } else if (opponentChoice === "spock") {
+      alert("Opponent chose spock YOU WIN");
+      scores++;
     }
-
-    if (userChoice === opponentChoice) {
-        alert("DRAW");
+  } else if (userChoice === "scissors") {
+    if (opponentChoice === "paper") {
+      alert("Opponent chose paper YOU WIN");
+      scores++;
+    } else if (opponentChoice === "rock") {
+      alert("Opponent chose rock YOU LOSE");
+      opponent++;
+    } else if (opponentChoice === "lizard") {
+      alert("Opponent chose lizard YOU WIN");
+      scores++;
+    } else if (opponentChoice === "spock") {
+      alert("Opponent chose spock YOU LOSE");
+      opponent++;
     }
-
-    else if (userChoice === 'rock') {
-        if (opponentChoice === 'paper') {
-            alert("Opponent chose paper YOU LOSE");
-            opponent++;
-        } else if (opponentChoice === 'scissors') {
-            alert('Opponent chose scissors YOU WIN');
-            scores++;
-        } else if (opponentChoice === 'lizard') {
-            alert('Opponent chose lizard YOU WIN');
-            scores++;
-        } else if (opponentChoice === 'spock') {
-            alert('Opponent chose spock YOU LOSE');
-            opponent++;
-        }
+  } else if (userChoice === "lizard") {
+    if (opponentChoice === "paper") {
+      alert("Opponent chose paper YOU WIN");
+      scores++;
+    } else if (opponentChoice === "scissors") {
+      alert("Opponent chose scissors YOU LOSE");
+      opponent++;
+    } else if (opponentChoice === "rock") {
+      alert("Opponent chose ROCK YOU LOSE");
+      opponent++;
+    } else if (opponentChoice === "spock") {
+      alert("Opponent chose spock YOU WIN");
+      scores++;
     }
-
-    else if (userChoice === 'paper') {
-        if (opponentChoice === 'rock') {
-            alert("Opponent chose rock YOU WIN");
-            scores++;
-        } else if (opponentChoice === 'scissors') {
-            alert('Opponent chose scissors YOU LOSE');
-            scores++;
-        } else if (opponentChoice === 'lizard') {
-            alert('Opponent chose lizard YOU LOSE');
-            opponent++;
-        } else if (opponentChoice === 'spock') {
-            alert('Opponent chose spock YOU WIN');
-            scores++;
-        }
+  } else if (userChoice === "spock") {
+    if (opponentChoice === "paper") {
+      alert("Opponent chose  paper YOU LOSE");
+      opponent++;
+    } else if (opponentChoice === "scissors") {
+      alert("Opponent chose scissors YOU WIN");
+      scores++;
+    } else if (opponentChoice === "lizard") {
+      alert("Opponent chose lizard YOU LOSE");
+      opponent++;
+    } else if (opponentChoice === "rock") {
+      alert("Opponent chose rock YOU WIN");
+      scores++;
     }
+  }
 
-    else if (userChoice === 'scissors') {
-        if (opponentChoice === 'paper') {
-            alert("Opponent chose paper YOU WIN");
-            scores++;
-        } else if (opponentChoice === 'rock') {
-            alert('Opponent chose rock YOU LOSE');
-            opponent++;
-        } else if (opponentChoice === 'lizard') {
-            alert('Opponent chose lizard YOU WIN');
-            scores++;
-        } else if (opponentChoice === 'spock') {
-            alert('Opponent chose spock YOU LOSE');
-            opponent++;
-        }
+  console.log(opponentChoice);
+  console.log(userChoice);
+
+  turns++;
+  document.getElementById("tries").textContent = turns;
+  document.getElementById("player-score").textContent = scores;
+  document.getElementById("opponentChoice").textContent = opponent;
+
+  if (turns === 5) {
+    if (scores >= 3) {
+      alert(`Opponent chose ${opponentChoice} GAME OVER YOU WIN !!`);
+    } else {
+      alert(`Opponent chose ${opponentChoice} GAME OVER YOU LOSE !!`);
     }
-
-    else if (userChoice === 'lizard') {
-        if (opponentChoice === 'paper') {
-            alert("Opponent chose paper YOU WIN");
-            scores++;
-        } else if (opponentChoice === 'scissors') {
-            alert('Opponent chose scissors YOU LOSE');
-            opponent++;
-        } else if (opponentChoice === 'rock') {
-            alert('Opponent chose ROCK YOU LOSE');
-            opponent++;
-        } else if (opponentChoice === 'spock') {
-            alert('Opponent chose spock YOU WIN');
-            scores++;
-        }
-    }
-
-    else if (userChoice === 'spock') {
-        if (opponentChoice === 'paper') {
-            alert("Opponent chose  paper YOU LOSE");
-            opponent++;
-        } else if (opponentChoice === 'scissors') {
-            alert('Opponent chose scissors YOU WIN');
-            scores++;
-        } else if (opponentChoice === 'lizard') {
-            alert('Opponent chose lizard YOU LOSE');
-            opponent++;
-        } else if (opponentChoice === 'rock') {
-            alert('Opponent chose rock YOU WIN');
-            scores++;
-        }
-    }
-
-
-    console.log(opponentChoice);
-    console.log(userChoice);
-
-    turns++;
-    document.getElementById("tries").textContent = turns;
-    document.getElementById("player-score").textContent = scores;
-    document.getElementById("opponentChoice").textContent = opponent;
-
-    if (turns === 5) {
-        if (scores >= 3) {
-            alert(`Opponent chose ${opponentChoice} GAME OVER YOU WIN !!`);
-        } else {
-            alert(`Opponent chose ${opponentChoice} GAME OVER YOU LOSE !!`);
-        }
-        replayGame();
-    }
+    replayGame();
+  }
 }
-
 
 /* Game replay */
 function replayGame() {
-    scores = 0;
-    turns = 0;
-    opponent = 0;
-    document.getElementById("tries").textContent = turns;
-    document.getElementById("player-score").textContent = scores;
-    document.getElementById("opponentChoice").textContent = opponent;
+  scores = 0;
+  turns = 0;
+  opponent = 0;
+  document.getElementById("tries").textContent = turns;
+  document.getElementById("player-score").textContent = scores;
+  document.getElementById("opponentChoice").textContent = opponent;
 }
